3b4129247e250dd3610e37f103c46407868d5898
Lyudmila Afanaseva <afanaseva_mila@vk.com>
2024-03-24 21:26:10 +0300

    add

---
+++ src/App.java
@@ -0,0 +1,54 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.println("Введите данные латиницей в следующем формате: Фамилия Имя Отчество дата_рождения номер_телефона пол");
+            String input = scanner.nextLine();
+            String[] data = input.split(" ");
+
+            if (data.length != 6) {
+                throw new IllegalArgumentException("Недопустимое количество данных!");
+            }
+
+            String lastName = data[0];
+            String firstName = data[1];
+            String middleName = data[2];
+            String birthDate = data[3];
+            long phoneNumber = Long.parseLong(data[4]);
+            char gender = data[5].charAt(0);
+
+            if (!birthDate.matches("\\d{2}.\\d{2}.\\d{4}")) {
+                throw new IllegalArgumentException("Неверный формат даты рождения!");
+            }
+
+            if (gender != 'f' && gender != 'm') {
+                throw new IllegalArgumentException("Неверно указан пол!");
+            }
+
+            String fileName = lastName + ".txt";
+            FileWriter fileWriter = new FileWriter(fileName, true);
+            fileWriter.write(lastName + " " + firstName + " " + middleName + " " + birthDate + " "
+                    + phoneNumber + " " + gender + "\n");
+            fileWriter.close();
+            System.out.println("Данные успешно записаны в файл " + fileName);
+
+        } catch (NumberFormatException e) {
+            System.out.println("Ошибка формата номера телефона.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Ошибка при записи в файл: ");
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("Произошла ошибка: " + e.getMessage());
+            e.printStackTrace();
+
+        } finally{
+            scanner.close();
+        }
+    }   
+}
